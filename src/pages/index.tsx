92e7b53265de7566c28060de05eee4495d4d9f61
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-12-31 10:04:38 -0300

    Prettier config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Hi</h1>
     </div>
   );
-}
+};
 
 export default Home;
