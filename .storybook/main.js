4b46e250c21d18f693d9fb35500159b1bad23750
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-04 14:51:26 -0300

    Add and configure Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-knobs",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
